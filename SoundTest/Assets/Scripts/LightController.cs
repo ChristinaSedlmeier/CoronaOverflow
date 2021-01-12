8c33808d16ac53d0aff92aef6417bc1e4fb793e1
Christina Sedlmeier <38252323+ChristinaSedlmeier@users.noreply.github.com>
2021-01-12 10:49:01 +0100

    Add Initial Version

---
+++ SoundTest/Assets/Scripts/LightController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightController : MonoBehaviour
+
+{
+
+    Material lightOn;
+    Material lightOff;
+
+    void Start()
+    {
+        lightOn = Resources.Load("Emission", typeof(Material)) as Material;
+        lightOff = Resources.Load("LightOff", typeof(Material)) as Material;
+    }
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        Debug.Log(collider);
+        if (collider.tag == "Player")
+        {
+            GetComponent<Renderer>().material = lightOn;
+            Debug.Log("is Player");
+        }
+    }
+
+    private void OnTriggerExit(Collider collider)
+    {
+        if (collider.tag == "Player")
+        {
+            GetComponent<Renderer>().material = lightOff;
+            Debug.Log("is Player");
+        }
+    }
+}
